fda7fe12daadbbf409157bf9ac36447465e6236c
Rujul13 <rujuljadav@gmail.com>
2026-04-18 01:16:29 -0400

    realtime polling  protobuf

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+// import { decodeFeed } from "./app/services/gtfsRealtime";
+import { useBusTracker } from "./app/viewmodels/useBusTracker";
 export default function App() {
+  // decodeFeed(
+  //   "https://s3.amazonaws.com/etatransit.gtfs/bloomingtontransit.etaspot.net/alerts.pb",
+  // ).
+  const { realtimeData, lastUpdated } = useBusTracker();
+  console.log("last updated:", lastUpdated);
+  then((d) => console.log(JSON.stringify(d, null, 2)));
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
